69378c570a613494a46c36f32b49843a831e9330
pinkypsy <pinkypsy@gmail.com>
2020-07-01 21:09:10 +0300

    test4

--- src/ua/alvin/Main.java
+++ src/ua/alvin/Main.java
@@ -6,6 +6,8 @@ public class Main {
         System.out.println("hi");
         System.out.println("hi");
         System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
         	// write your code herorigine
     }
 }
